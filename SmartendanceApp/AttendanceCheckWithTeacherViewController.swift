744dc6dbca05402ebf846aa0f82072820dfd5361
ryota-k0827 <ryota171124@icloud.com>
2021-06-28 02:14:18 +0900

    add

---
+++ SmartendanceApp/AttendanceCheckWithTeacherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AttendanceCheckWithTeacherViewController.swift
+//  SmartendanceApp
+//
+//  Created by Ryota Kaneko on 2021/06/28.
+//
+
+import UIKit
+
+class AttendanceCheckWithTeacherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
