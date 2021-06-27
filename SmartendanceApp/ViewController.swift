5b2c80de98b9ee9b728e7cc6d2c831a91e1db3f9
ryota-k0827 <ryota171124@icloud.com>
2021-06-27 23:37:53 +0900

    Initial Commit

---
+++ SmartendanceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartendanceApp
+//
+//  Created by Ryota Kaneko on 2021/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
